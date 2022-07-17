5c9432f1dcd3c2d1f90ca837155e543d4d5eb6d0
Oby <obyli214@gmail.com>
2022-07-16 18:55:58 -0700

    implement Home page and added current user in profile page

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')

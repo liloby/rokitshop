40a1cab09b93a0515949ecee5e8061a705999369
Oby <obyli214@gmail.com>
2022-07-18 15:08:16 -0700

    added filter item on index page

--- main_app/views.py
+++ main_app/views.py
@@ -73,4 +73,4 @@ def add_post(request, item_id):
   print(request, item)
   item.posted = True
   item.save()
-  return redirect('profile', item_id=item_id)
+  return redirect('index', item_id=item_id)

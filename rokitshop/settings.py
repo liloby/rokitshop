6d6b49bcddf634435e17fafb74175859b6d4bcd1
Kiefer Irvine <kieferpirvine@gmail.com>
2022-07-18 11:36:59 -0600

    Adjust line 117 in Settings

--- rokitshop/settings.py
+++ rokitshop/settings.py
@@ -114,7 +114,8 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 LOGIN_REDIRECT_URL = '/profile/'
 
